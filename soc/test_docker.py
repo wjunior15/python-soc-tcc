fa2a8e2116cda3f096c2037f506736f8c3e71580
Wellington A <wellingtonresende15@gmail.com>
2024-10-17 22:57:36 -0300

    versao final erro de leitura da rede do app

--- soc/test_docker.py
+++
@@ -1,9 +0,0 @@
-import os
-print("Inicia Container")
-
-cont = 0
-env_var = os.getenv("DB_USER")
-
-while 1:
-    print("Container continua rodando",cont,"e pegando variaveis de ambiente",env_var)
-    cont += 1
