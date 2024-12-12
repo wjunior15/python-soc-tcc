3e14e9c7942097575229d66d386fca17e9f5400e
Wellington A <wellingtonresende15@gmail.com>
2024-12-12 15:52:47 -0300

    implementa endpoint login

--- app/app.py
+++ app/app.py
@@ -8,6 +8,9 @@ app_port = int(os.getenv("APP_PORT"))
 redis_host = str(os.getenv("REDIS_HOST"))
 redis_port = int(os.getenv("REDIS_PORT"))
 
+APP_USER = 'admin'
+APP_PASSWORD = 'senha123'
+
 app = Flask(__name__)
 CORS(app)
 
@@ -31,5 +34,16 @@ def sum():
         
     return jsonify({'message':'dados faltantes'})
 
+@app.route('/login', methods=['POST'])
+def login():
+    # Get username and password from the request
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    # Check if the username and password are correct
+    if username == APP_USER and password == APP_PASSWORD:
+        return jsonify({"status": "success", "message": "Login successful!"}), 200
+    else:
+        return jsonify({"status": "failure", "message": "Invalid credentials."}), 401
 
 app.run(host=app_host, port=app_port, debug=True)
